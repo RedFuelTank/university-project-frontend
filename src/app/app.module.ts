d62348dc037a07b691c1ae451d5dd8f4048d67b3
kitimo <kitimo@ttu.ee>
2021-10-01 15:40:54 +0300

    structure is created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,27 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AdvertismentComponent } from './advertisment/advertisment.component';
+import { AuthorComponent } from './author/author.component';
+import { AuthorsComponent } from './authors/authors.component';
+import { AdvertisementsComponent } from './advertisements/advertisements.component';
+import { InterfaceComponent } from './interface/interface.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AdvertismentComponent,
+    AuthorComponent,
+    AuthorsComponent,
+    AdvertisementsComponent,
+    InterfaceComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  exports: []
 })
 export class AppModule { }
