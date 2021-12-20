93c515a863d87cff0d3953051b5edcfb87686e28
Kirill Timofejev <kitimo@ttu.ee>
2021-12-20 23:58:29 +0200

    Spinner changes

--- src/app/advertisements/advertisements.component.ts
+++ src/app/advertisements/advertisements.component.ts
@@ -1,53 +1,54 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {Advertisement} from "../shared/advertisement";
 import {AdvertisementsService} from "../shared/advertisements.service";
 import {ActivatedRoute, Router} from "@angular/router";
 
 @Component({
-  selector: 'app-advertisements',
-  templateUrl: './advertisements.component.html',
-  styleUrls: ['./advertisements.component.css']
+    selector: 'app-advertisements',
+    templateUrl: './advertisements.component.html',
+    styleUrls: ['./advertisements.component.css']
 })
 export class AdvertisementsComponent implements OnInit {
-  public advertisements: Advertisement[] = [];
-  public advertisementsOnPage: Advertisement[] = [];
-  currentState: string = "";
-  public currentPage : number = 1;
+    public advertisements: Advertisement[] = [];
+    public advertisementsOnPage: Advertisement[] = [];
+    currentState: string = "";
+    public currentPage: number = 1;
 
-  constructor(private service: AdvertisementsService, private activatedRoute: ActivatedRoute, private route: Router) {
-  }
+    constructor(private service: AdvertisementsService, private activatedRoute: ActivatedRoute, private route: Router) {
+    }
 
-  goToAdvertisementFormPage(){
-    this.route.navigate(['/advertisement-form']);
-  }
+    goToAdvertisementFormPage() {
+        this.route.navigate(['/advertisement-form']);
+    }
 
-  getUrl(ad: Advertisement) {
-    this.route.navigate(["/" + this.currentState + "/" + ad.id]);
-  }
+    getUrl(ad: Advertisement) {
+        this.route.navigate(["/" + this.currentState + "/" + ad.id]);
+    }
 
-  ngOnInit(): void {
-    this.loadAdvertisements();
-  }
+    ngOnInit(): void {
+        this.loadAdvertisements();
+    }
 
-  public loadAdvertisements() {
-    this.activatedRoute.url.subscribe((url) => {
-      if (url.join("").toString().includes("requests")) {
-        this.currentState = "requests";
-        this.loadRequests(this.currentPage);
-      } else {
-        this.currentState = "offers";
-        this.loadOffers(this.currentPage);
-      }
-    })
-  }
+    public loadAdvertisements() {
+        this.advertisementsOnPage = []
+        this.activatedRoute.url.subscribe((url) => {
+            if (url.join("").toString().includes("requests")) {
+                this.currentState = "requests";
+                this.loadRequests(this.currentPage);
+            } else {
+                this.currentState = "offers";
+                this.loadOffers(this.currentPage);
+            }
+        })
+    }
 
-  public loadOffers(page : number) {
-    this.service.getOffers().subscribe(advertisement => this.advertisements = advertisement);
-    this.service.getOfferPage(page).subscribe(advertisement => this.advertisementsOnPage = advertisement);
-  }
+    public loadOffers(page: number) {
+        this.service.getOffers().subscribe(advertisement => this.advertisements = advertisement);
+        this.service.getOfferPage(page).subscribe(advertisement => this.advertisementsOnPage = advertisement);
+    }
 
-  public loadRequests(page : number) {
-    this.service.getRequests().subscribe(advertisement => this.advertisements = advertisement);
-    this.service.getRequestPage(page).subscribe(advertisement => this.advertisementsOnPage = advertisement);
-  }
+    public loadRequests(page: number) {
+        this.service.getRequests().subscribe(advertisement => this.advertisements = advertisement);
+        this.service.getRequestPage(page).subscribe(advertisement => this.advertisementsOnPage = advertisement);
+    }
 }
